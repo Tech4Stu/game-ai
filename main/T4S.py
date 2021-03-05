9292f522ac0773980ef23d3f4ce8b44967e8bd4b
Viktor Lambert <viktor.lambert@icloud.com>
2021-03-05 12:08:10 +0100

    collision detection en force visualiser

--- main/T4S.py
+++ main/T4S.py
@@ -19,7 +19,7 @@ class krachten:
         self.x_dist = x_dist
         self.y_dist = y_dist
         self.__Fx = self.F*math.cos(self.hoek)
-        self.__Fy = self.F*math.sin(self.hoek)
+        self.__Fy = -self.F*math.sin(self.hoek)
         self.__M = self.__Fy*abs(self.x_dist) + self.__Fx*abs(self.y_dist)   #pos is tegenklok
 
         #return self.Fx, self.Fy, self.M
@@ -57,6 +57,8 @@ class krachten:
         result.set_all(added_Fx, added_Fy, added_M)
         return result
 
+    def draw(self,car):
+        pygame.draw.line(screen,black,(car.centerx+self.x_dist,car.centery+self.y_dist),(car.centerx+self.x_dist+self.__Fx,car.centery+self.y_dist+self.__Fy),10)
 
 class Car:
     def __init__(self, x, y, width, height, mass):
@@ -71,32 +73,42 @@ class Car:
         self.x_a = 0
         self.y_a = 0
         self.I = self.width * self.height * self.height * self.height / 12 #traagheidsmoment bh³/12
-        self.hoek = 0 #radialen
+        self.hoek = 0.05 #radialen
         self.omega = 0
         self.alpha = 0
         #hoekpunten bepalen
         self.L = math.sqrt((self.height*self.height+self.width*self.width)/4) #lengte van center -> hoekpunten
         self.hoekpunthoek = math.atan2(self.height, self.width) #geeft hoek van center -> hoekpunten atan2 == atan(y/x)
         #de vier lijnen voor collision
-        self.Lline = Line(1)    #left line
-        self.Rline = Line(1)    #right line
-        self.Tline = Line(1)    #top line
-        self.Bline = Line(1)    #bottom line
 
-    def draw(self):
+        self.collisionbox = []
+
         alpha1 = math.pi - self.hoekpunthoek + self.hoek
         alpha2 = self.hoekpunthoek + self.hoek
         alpha3 = -self.hoekpunthoek + self.hoek
         alpha4 = math.pi + self.hoekpunthoek + self.hoek
-        x1 = self.L * math.cos(alpha1) + self.centerx
-        y1 = self.L * math.sin(alpha1) + self.centery
-        x2 = self.L * math.cos(alpha2) + self.centerx
-        y2 = self.L * math.sin(alpha2) + self.centery
-        x3 = self.L * math.cos(alpha3) + self.centerx
-        y3 = self.L * math.sin(alpha3) + self.centery
-        x4 = self.L * math.cos(alpha4) + self.centerx
-        y4 = self.L * math.sin(alpha4) + self.centery
-        pygame.draw.polygon(screen, red, ((x1,y1),(x2,y2),(x3,y3),(x4,y4)), 3)
+        self.x1 = self.L * math.cos(alpha1) + self.centerx
+        self.y1 = self.L * math.sin(alpha1) + self.centery
+        self.x2 = self.L * math.cos(alpha2) + self.centerx
+        self.y2 = self.L * math.sin(alpha2) + self.centery
+        self.x3 = self.L * math.cos(alpha3) + self.centerx
+        self.y3 = self.L * math.sin(alpha3) + self.centery
+        self.x4 = self.L * math.cos(alpha4) + self.centerx
+        self.y4 = self.L * math.sin(alpha4) + self.centery
+        self.collisionbox.append(Line(self.x1, self.y1, self.x2, self.y2))
+        self.collisionbox.append(Line(self.x2, self.y2, self.x3, self.y3))
+        self.collisionbox.append(Line(self.x3, self.y3, self.x4, self.y4))
+        self.collisionbox.append(Line(self.x4, self.y4, self.x1, self.y1))
+
+    def draw(self):
+        self.collisionbox = []
+        self.collisionbox.append(Line(self.x1, self.y1, self.x2, self.y2))
+        self.collisionbox.append(Line(self.x2, self.y2, self.x3, self.y3))
+        self.collisionbox.append(Line(self.x3, self.y3, self.x4, self.y4))
+        self.collisionbox.append(Line(self.x4, self.y4, self.x1, self.y1))
+        for line in self.collisionbox:
+            line.draw()
+
 
 
     def update(self, krachtenlijst):
@@ -105,21 +117,35 @@ class Car:
         :param krachten: krachten lijst , zijnde Fz, Fgas, Frem, Fcoll, (Fvering)
         :return: /
         '''
-        total_F = krachten(0,0,0,0) #is een object krachten() die de som van alle ingegeven/inwerkende krachten op de auto
-        for kracht in krachtenlijst:
-            total_F += kracht
-        print(total_F.get_all())
-        #somF = m*a -> x_a = Fx/m  // y_a = Fy/m
-        self.x_a = total_F.get_Fx()/self.mass
-        self.x_v = 1
-        self.centerx = 1
-        self.y_a = total_F.get_Fy()/self.mass
-        self.y_v = 1
-        self.centery = 1
-        #somM = I*alpha
-        self.alpha = total_F.get_M()/self.I
-        self.omega = 1
-        self.hoek = 1
+        if 0:  #tijdelijk afzetten voor test collision
+            total_F = krachten(0,0,0,0) #is een object krachten() die de som van alle ingegeven/inwerkende krachten op de auto
+            for kracht in krachtenlijst:
+                total_F += kracht
+            print(total_F.get_all())
+            #somF = m*a -> x_a = Fx/m  // y_a = Fy/m
+            self.x_a = total_F.get_Fx()/self.mass
+            self.x_v = 1
+            self.centerx = 1
+            self.y_a = total_F.get_Fy()/self.mass
+            self.y_v = 1
+            self.centery = 1
+            #somM = I*alpha
+            self.alpha = total_F.get_M()/self.I
+            self.omega = 1
+            self.hoek = 1
+
+        alpha1 = math.pi - self.hoekpunthoek + self.hoek
+        alpha2 = self.hoekpunthoek + self.hoek
+        alpha3 = -self.hoekpunthoek + self.hoek
+        alpha4 = math.pi + self.hoekpunthoek + self.hoek
+        self.x1 = self.L * math.cos(alpha1) + self.centerx
+        self.y1 = self.L * math.sin(alpha1) + self.centery
+        self.x2 = self.L * math.cos(alpha2) + self.centerx
+        self.y2 = self.L * math.sin(alpha2) + self.centery
+        self.x3 = self.L * math.cos(alpha3) + self.centerx
+        self.y3 = self.L * math.sin(alpha3) + self.centery
+        self.x4 = self.L * math.cos(alpha4) + self.centerx
+        self.y4 = self.L * math.sin(alpha4) + self.centery
 
 #KLEUREN
 black   = (0,0,0)
@@ -172,21 +198,17 @@ class Perlin:
     def __lerp(self, start, stop, amt):
         return amt*(stop-start)+start
 
-
-#line class
 class Line:
-    def __init__(self,index):
-        self.index = index
-        self.color = black
-        #FUNCTIE VOOR HOOGTE
-        self.beginy = WINDOW_SIZE[1]/2 + noise.valueAt(index)*WINDOW_SIZE[1]/3
-        self.endy = WINDOW_SIZE[1]/2 + noise.valueAt(index+1)*WINDOW_SIZE[1]/3
+    def __init__(self,beginx,beginy,endx,endy):
+        self.beginx = beginx
+        self.beginy = beginy
+        self.endx = endx
+        self.endy = endy
         self.width = 5
+        self.color = red
 
-    def draw(self,centerindex):
-        self.beginx = WINDOW_SIZE[0]/segments * (self.index + segments/2 - centerindex)
-        self.endx = WINDOW_SIZE[0]/segments * (self.index + (segments/2)+1 - centerindex)
-        pygame.draw.line(screen,self.color,(self.beginx,self.beginy),(self.endx,self.endy),self.width)
+    def draw(self):
+        pygame.draw.line(screen, self.color, (self.beginx, self.beginy), (self.endx, self.endy), self.width)
 
     def collide(self,line):
         x1 = self.beginx
@@ -207,39 +229,54 @@ class Line:
         c = False
         d = False
 
-        kleinste = x1
-        grootste = x2
+        if x1 >= x2:
+            if x2 <= x and x <= x1:
+                a = True
+        else:
+            if x1 <= x and x <= x2:
+                a = True
 
-        if x1 > x2:
-            grootste = x1
-            kleinste = x2
-
-        if kleinste < x < grootste:
-            a = True
-
-        if x3 > x4:
-            if x4 < x and x < x3:
+        if x3 >= x4:
+            if x4 <= x and x <= x3:
                 b = True
         else:
-            if x3 < x and x < x4:
+            if x3 <= x and x <= x4:
                 b = True
 
-        if y1 > y2:
-            if y2 < y and y < y1:
+        if y1 >= y2:
+            if y2 <= y and y <= y1:
                 c = True
         else:
-            if y1 < y and y < y2:
+            if y1 <= y and y <= y2:
                 c = True
 
-        if y3 > y4:
-            if y4 < y and y < y3:
+        if y3 >= y4:
+            if y4 <= y and y <= y3:
                 d = True
         else:
-            if y3 < y and y < y4:
+            if y3 <= y and y <= y4:
                 d = True
+
 
         if a and b and c and d:
-            pygame.draw.circle(gameDisplay, (255, 0, 0), (x, y), 10)
+            pygame.draw.circle(screen, (0, 0, 255), (x, y), 10)
+            return (x,y)
+
+#roadsegment class
+class Roadsegment:
+    def __init__(self,index):
+        self.index = index
+        self.color = black
+        #FUNCTIE VOOR HOOGTE
+        self.beginy = WINDOW_SIZE[1]/2 + noise.valueAt(index)*WINDOW_SIZE[1]/3
+        self.endy = WINDOW_SIZE[1]/2 + noise.valueAt(index+1)*WINDOW_SIZE[1]/3
+        self.width = 5
+
+    def draw(self,centerindex):
+        self.beginx = WINDOW_SIZE[0]/segments * (self.index + segments/2 - centerindex)
+        self.endx = WINDOW_SIZE[0]/segments * (self.index + (segments/2)+1 - centerindex)
+        pygame.draw.line(screen,self.color,(self.beginx,self.beginy),(self.endx,self.endy),self.width)
+
 
 #map maken in lijst terrein
 
@@ -265,11 +302,11 @@ def game_loop():
     '''
     latch = 1
     t = 0
-    car = Car(WINDOW_SIZE[0]//4, 100, 100, 50, 5)
+    car = Car(WINDOW_SIZE[0]//2, 300, 100, 50, 5)
     g = 9.81
     Fz = krachten(car.mass*g, -math.pi/2, 0, 0)                 #zwaartekracht m*g
-    Fgas = krachten(100, 0, -car.width/2, -car.height/2)        #gaskracht waarde kan bepaald worden nu gwn 100
-    Frem = krachten(100, math.pi, -car.width/2, -car.height/2)  #remkracht idem
+    Fgas = krachten(100, 0, -car.width/2, car.height/2)        #gaskracht waarde kan bepaald worden nu gwn 100
+    Frem = krachten(100, math.pi, -car.width/2, car.height/2)  #remkracht idem
     Fcoll = krachten(0,0,0,0)                                   #collision kracht
     left = False
     right = False
@@ -307,17 +344,26 @@ def game_loop():
             # In en uitladen terrain
             if segment.endx <= 0:
                 road.remove(segment)
-                road.append(Line(segment.index + segments))
+                road.append(Roadsegment(segment.index + segments))
             if segment.beginx >= WINDOW_SIZE[0]:
                 road.remove(segment)
-                road.append(Line(segment.index - segments))
+                road.append(Roadsegment(segment.index - segments))
         #auto handelen
         krachtenlijst.append(Fz)
+
+        for stuk in road:
+            for line in car.collisionbox:
+                line.collide(stuk)
+
         krachtenlijst.append(Fcoll)
+
 
         car.update(krachtenlijst)
         car.draw()
 
+        #krachten tekenen
+        for kracht in krachtenlijst:
+            kracht.draw(car)
 
 
         pygame.display.update()
@@ -400,14 +446,18 @@ settings_button = engine.Button(screen,
 #parameters
 hoek = 0 #hoek waarrond titel wordt gedraaid
 sign = 0.25 #dhoek/dt
+
+#resolutie road
 segments = 50
+
+
 #Noise klaar zetten
 noise = Perlin(500//segments) ## frequentie terrain
 #road initialiseren
 road = []
 i = 0
 for segment in range(segments):
-    road.append(Line(i))
+    road.append(Roadsegment(i))
     i+=1
 run = True
 while run:
