857afca2d1ae59babbe08e4be820175edba8d185
Xander Warszawski <xander.warszawski@outlook.com>
2021-03-28 11:50:25 +0200

    Trying to get repo data

---
+++ docs/assets/js/getRepoData.js
@@ -0,0 +1,9 @@
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` });
+
+const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+  owner: 'Tech4Stu',
+  repo: 'hillclimber'
+});
