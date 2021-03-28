5d79ddf4b9cef49c1e8c5b083ee2ea335d968795
Xander Warszawski <xander.warszawski@outlook.com>
2021-03-28 12:09:10 +0200

    Trying to make it work

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,11 +1,12 @@
-(async function(){
-  // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-  const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` });
+const { Octokit } = require("@octokit/rest");
 
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` })
+
+(async () => {
   const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
     owner: 'Tech4Stu',
     repo: 'hillclimber'
-  });
-  
-  document.getElementById("data-test").innerHTML = commits;
-})()
+  })
+})
+document.getElementById("data-test").innerHTML = commits
