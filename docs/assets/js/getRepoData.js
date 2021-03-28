2cfdecaba61d4533ddc6ae3bdf300eeb95cd42d9
Xander Warszawski <xander.warszawski@outlook.com>
2021-03-28 12:00:49 +0200

    Async problem

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,9 +1,10 @@
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` });
 
-const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-  owner: 'Tech4Stu',
-  repo: 'hillclimber'
-});
-
+(async function(){
+  const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+    owner: 'Tech4Stu',
+    repo: 'hillclimber'
+  });
+})()
 document.getElementById("data-test").innerHTML = commits;
