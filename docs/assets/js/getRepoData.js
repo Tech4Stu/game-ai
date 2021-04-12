63b90e52a82aa14e5e585c47ea1e548eee0550f9
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-12 21:40:13 +0200

    Trying to figure out the docs

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,10 +1,12 @@
 import { createTokenAuth } from "https://cdn.skypack.dev/@octokit/auth-token";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
+const TOKEN = "f6d70d34445424b440bb17ca322215b7db9f6a26";
+
 async function getCommits() {
-  const auth = createTokenAuth("f6d70d34445424b440bb17ca322215b7db9f6a26");
+  const auth = createTokenAuth(TOKEN);
   const authentication = await auth();
-  const octokit = new Octokit({auth: authentication});
+  const octokit = new Octokit({options.auth: authentication});
 
   console.log("Async started");
   const commits = await octokit.rest.repos.listCommits({
