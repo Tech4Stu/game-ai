d10e3985e2e1229bdb1ad4cab250f789689ebd5a
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-12 17:35:23 +0200

    Still trying to use Octokit

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,5 +1,8 @@
+<script type="module">
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` })
+const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` });
 
 (async () => {
   const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
@@ -8,3 +11,4 @@ const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` }
   })
 })
 console.log(commits)
+</script>
