2cb7cd3d64c0a007f501c6dd9b331cb0c8c78267
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-12 21:25:40 +0200

    Await problem

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,11 +1,11 @@
 import { createTokenAuth } from "https://cdn.skypack.dev/@octokit/auth-token";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
-const auth = createTokenAuth("f6d70d34445424b440bb17ca322215b7db9f6a26");
-const authentication = await auth();
-const octokit = new Octokit({auth: authentication});
-
 async function getCommits() {
+  const auth = createTokenAuth("f6d70d34445424b440bb17ca322215b7db9f6a26");
+  const authentication = await auth();
+  const octokit = new Octokit({auth: authentication});
+
   console.log("Async started");
   const commits = await octokit.rest.repos.listCommits({
     owner: "Tech4Stu",
