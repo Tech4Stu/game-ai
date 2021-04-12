6fea754a56ad74239846f56774376b5cb8c53b05
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-12 19:43:10 +0200

    Maybe this works

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -3,11 +3,16 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({auth: "0459c1c341c8b7b6a9f7a79a05ab266e629cbaad"});
 
+const auth = await octokit.request("/user");
+
 (async () => {
-  const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-    owner: 'Tech4Stu',
-    repo: 'hillclimber'
-  });
+  const commits = await octokit.rest.commits.get({
+  owner: "Tech4Stu",
+  repo: "hillclimber"",
+  mediaType: {
+    format: "patch",
+  },
+});
   console.log("Async is working");
   console.log(commits);
 });
