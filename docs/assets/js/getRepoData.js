4fccff9ee5e5babc22db2b4f8b28b5f75ee37ba8
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-12 17:47:39 +0200

    new... is not a function

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,12 +1,12 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-const octokit = new Octokit({ auth: `0459c1c341c8b7b6a9f7a79a05ab266e629cbaad` })
+const octokit = new Octokit({auth: "0459c1c341c8b7b6a9f7a79a05ab266e629cbaad"});
 
 (async () => {
   const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
     owner: 'Tech4Stu',
     repo: 'hillclimber'
-  })
-  console.log(commits)
-})
+  });
+  console.log(commits);
+});
