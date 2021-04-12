185a1df084b3f26af1d7247bfad925d53c029b5f
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-12 19:53:42 +0200

    Found the error

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -3,16 +3,14 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({auth: "0459c1c341c8b7b6a9f7a79a05ab266e629cbaad"});
 
-const auth = await octokit.request("/user");
-
 (async () => {
   const commits = await octokit.rest.commits.get({
-  owner: "Tech4Stu",
-  repo: "hillclimber",
-  mediaType: {
-    format: "patch",
-  },
+    owner: "Tech4Stu",
+    repo: "hillclimber",
+    mediaType: {
+      format: "patch",
+    },
+  });
+  console.log("Async is working");
+  console.log(commits);
 });
-});
-console.log("Async is working");
-console.log(commits);
