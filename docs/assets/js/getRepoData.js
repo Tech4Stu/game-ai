cdd58741974747dc5b2d7f1d2319631b37006b72
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-13 20:29:57 +0200

    Bad credentials

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,7 +1,7 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
 async function getCommits(var_authentication) {
-  const octokit = new Octokit({auth: "f6d70d34445424b440bb17ca322215b7db9f6a26"});
+  const octokit = new Octokit({auth: "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"});
 
   console.log("Async started");
   const commits = await octokit.rest.repos.listCommits({
