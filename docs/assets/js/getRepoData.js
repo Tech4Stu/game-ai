121f3b81d5ebb239e8a3a49d4cc3a3894747f2a9
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-13 18:27:17 +0200

    Is the auth included?

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,15 +1,7 @@
-import { createTokenAuth } from "https://cdn.skypack.dev/@octokit/auth-token";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
-async function authenticate(){
-  const TOKEN = "f6d70d34445424b440bb17ca322215b7db9f6a26";
-  const auth = createTokenAuth(TOKEN);
-  const authentication = await auth();
-  return authentication
-}
-
 async function getCommits(var_authentication) {
-  const octokit = new Octokit({auth: var_authentication});
+  const octokit = new Octokit({auth: "f6d70d34445424b440bb17ca322215b7db9f6a26"});
 
   console.log("Async started");
   const commits = await octokit.rest.repos.listCommits({
@@ -17,7 +9,7 @@ async function getCommits(var_authentication) {
     repo: "hillclimber",
   });
   console.log("Async finished");
-  return commits
+  return commits;
 }
 
-console.log(authenticate());
+console.log(getCommits());
