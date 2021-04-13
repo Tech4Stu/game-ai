8ca47617021e0cbb9b5dabd33bf0355a3038b19d
Xander Warszawski <xander.warszawski@outlook.com>
2021-04-13 17:34:42 +0200

    Still trying

--- docs/assets/js/getRepoData.js
+++ docs/assets/js/getRepoData.js
@@ -1,13 +1,15 @@
 import { createTokenAuth } from "https://cdn.skypack.dev/@octokit/auth-token";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
-
-const TOKEN = "f6d70d34445424b440bb17ca322215b7db9f6a26";
-const auth = createTokenAuth(TOKEN);
-
-async function getCommits() {
+async function authenticate(){
+  const TOKEN = "f6d70d34445424b440bb17ca322215b7db9f6a26";
+  const auth = await createTokenAuth(TOKEN);
   const authentication = await auth();
-  const octokit = new Octokit({auth: authentication});
+  return authentication
+}
+
+async function getCommits(var_authentication) {
+  const octokit = new Octokit({auth: var_authentication});
 
   console.log("Async started");
   const commits = await octokit.rest.repos.listCommits({
@@ -18,5 +20,5 @@ async function getCommits() {
   return commits
 }
 
-
-console.log(getCommits());
+authenticated = authenticate();
+console.log(getCommits(authenticated));
